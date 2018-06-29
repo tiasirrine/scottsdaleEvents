edff6f000702e8af8719015e88d18f5b3d118db3
Josh <jhint2424@gmail.com>
2018-06-28 18:03:33 -0700

    added logic for checking password

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,14 +13,13 @@ module.exports = {
   },
 
   // find user at login
-  findOne: function(req, res) {
-    // req.body holds the username and password from attempted sign in
-    // find potential user by search for the provided name off of req.body.name
-    User.findOne({ username: req.body.name })
+  findOne: function(user, password) {
+    User.findOne({ username: user })
       .then(returnedUser => {
-        // if a user is found,
-        // then call this.checkPassword to check the provided password
-        // pass in the potential password from the user, and the hashedPassword from the returnedUser
+        console.log(returnedUser);
+        this.checkPassword('Hinton', returnedUser.password)
+          .then(res => console.log('RESULT:', res))
+          .catch(err => console.log('ERROR:', err));
       })
       .catch(err => console.log(err));
   },
@@ -80,8 +79,16 @@ module.exports = {
   },
 
   checkPassword: function(potentialPassword, hashedPassword) {
-    // if the passwords match,
-    // then authenticate the user
-    // else, return login failed message to the user
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(potentialPassword, hashedPassword, function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        if (res === true) {
+          resolve(res);
+        } else {
+          reject('passwords do not match');
+        }
+      });
+    });
   }
 };
