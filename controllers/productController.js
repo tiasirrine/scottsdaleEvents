f750adfca13c64ff2dc761b51447a8e549080338
Trevor Johnson <johnsontrevor55@gmail.com>
2018-06-30 11:20:27 -0700

    updated userController.

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,23 +2,22 @@ const Product = require('../models/Product');
 
 module.exports = {
   // creates new product
-
-  create: function(req, res) {
-    Product.create(req.body)
+  create: function(product) {
+    Product.create(product)
       .then(result => console.log(result))
       .catch(err => console.log(err));
   },
 
   // finds all products
-  findAll: function(req, res) {
+  findAll: function() {
     Product.find({})
       .then(result => console.log(result))
       .catch(err => console.log(err));
   },
 
   // deletes a product
-  delete: function(req, res) {
-    Product.deleteOne(req.body)
+  delete: function(product) {
+    Product.deleteOne(product)
       .then(result => console.log(result))
       .catch(err => console.log(err));
   }
