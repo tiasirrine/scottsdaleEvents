c4b833ebb2cb497148c4bbfe7df10410b7195c4d
Josh <jhint2424@gmail.com>
2018-07-01 23:19:46 -0700

    adding simple login form

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { StripeProvider } from 'react-stripe-elements';
 import Checkout from './components/Checkout';
 import Home from './components/Pages/Home/Home';
+import Login from './components/Pages/Login/Login';
 require('dotenv').config();
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/adminlogin" component={Login} />
       <Route exact path="/checkout">
         <StripeProvider apiKey="pk_test_0I6cwW4VHLLkWbRqP53QRu8A">
           <Checkout />
