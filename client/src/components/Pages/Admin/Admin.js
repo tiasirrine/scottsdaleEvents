5f0d707000578a44497f64a96accdd3e8ceb6562
Trevor Johnson <johnsontrevor55@gmail.com>
2018-07-06 21:55:03 -0700

    starting over.

--- client/src/components/Pages/Admin/Admin.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Admin = () => <h3>Welcome, {sessionStorage.userName}</h3>;
-
-export default Admin;
--- models/Event.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const ProductSchema = new Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true }
-});
-
-const Product = mongoose.model('Product', ProductSchema);
