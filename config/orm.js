acf2f85425ea3c054c4c4518bc0280d11a189f8e
Trevor Johnson <johnsontrevor55@gmail.com>
2018-07-10 21:29:58 -0700

    created user model and orm functions.

--- config/orm.js
+++ config/orm.js
@@ -3,22 +3,51 @@ const connection = require('./connection');
 module.exports = {
   selectDistinct: function(table, callback) {
     const query = 'SELECT DISTINCT CATEGORY FROM ??;';
-    connection.query(query, [table], function(err, result) {
+    connection.query(query, [table], (err, result) => {
       callback(err, result);
     });
   },
 
-  selectAllByColumnValue: function(table, column, value, callback) {
+  selectAll: function(table, column, value, callback) {
     const query = 'SELECT * FROM ?? WHERE ??=?;';
-    connection.query(query, [[table], [column], [value]], function(
-      err,
-      result
-    ) {
+    connection.query(query, [[table], [column], [value]], (err, result) => {
       callback(err, result);
     });
   },
 
-  createOne: function(table, column, value, callback) {
-    const query = 'SELECT ';
+  createOneRow: function(table, columns, value, callback) {
+    const query = 'INSERT INTO ?? set ?? = ? ';
+    connection.query(query, [
+      [table],
+      [columns],
+      [value],
+      (err, result) => {
+        callback(err, result);
+      }
+    ]);
+  },
+
+  deleteOneRow: function(table, column, value, callback) {
+    const query = 'DELETE FROM ?? WHERE ??=?; ';
+    connection.query(query, [
+      [table],
+      [column],
+      [value],
+      (err, result) => {
+        callback(err, result);
+      }
+    ]);
+  },
+
+  updateOneRow: function(table, column, value, callback) {
+    const query = 'UPDATE ?? SET ?? WHERE ?';
+    connection.query(query, [
+      [table],
+      [column],
+      [value],
+      (err, result) => {
+        callback(err, result);
+      }
+    ]);
   }
 };
