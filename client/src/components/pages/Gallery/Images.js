1c2c05b3ef414bc62c1f255de3d6ff88a7744df2
Wing Wong <wing.wong@carvana.com>
2018-07-11 16:21:40 -0700

    Images updated to images

--- client/src/components/pages/Gallery/Images.js
+++ client/src/components/pages/Gallery/Images.js
@@ -1,10 +1,12 @@
 //// place holder for images till we get images from customer
 
-import light from '../../../Images/bulbstring.jpg';
-import light2 from '../../../Images/lightstring.jpg';
-import bar from '../../../Images/bar.jpg';
+import image1 from "../../../images/bulbstring.jpg";
+import image2 from "../../../images/lightstring.jpg";
+import image3 from "../../../images/bar.jpg";
+import image4 from "../../../images/blurrywood.jpg";
+import image5 from "../../../images/brick.jpg";
 
-const images = [light, light2, bar];
+const images = [image1, image2, image3, image4, image5];
 
 export default images;
 
