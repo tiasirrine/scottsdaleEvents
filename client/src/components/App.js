1794c5ca4072ff0b2f7c9c946440c01c48cde832
Josh <jhint2424@gmail.com>
2018-07-13 23:14:01 -0700

    added simple html to about
    Former-commit-id: 0b19040ba1df5be1b38b897b6b4dc48d13b93da3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,14 @@
-import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import Home from "./pages/Home";
-import InventoryPage from "./pages/Inventory";
-import API from "../api/API";
-import Gallery from "./pages/Gallery";
-import ContactUs from "./pages/Contact/Form/index";
-import CustomerLogin from "./pages/CustomerLogin";
-import ShoppingCart from "./pages/ShoppingCart";
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './pages/Home';
+import InventoryPage from './pages/Inventory';
+import API from '../api/API';
+import Gallery from './pages/Gallery';
+import ContactUs from './pages/Contact/Form/index';
+import CustomerLogin from './pages/CustomerLogin';
+import AboutPage from './pages/About';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends Component {
   constructor(props) {
@@ -20,12 +21,12 @@ class App extends Component {
   loadCategories = () => {
     return API.getDistinctCategories()
       .then(result => {
-        const arr = result.data.map(index => index["category"]);
+        const arr = result.data.map(index => index['category']);
         return this.setState({ categories: arr });
       })
       .catch(error => {
         console.error(error);
-        this.setState({ error: "500 (Internal Server Error)" });
+        this.setState({ error: '500 (Internal Server Error)' });
       });
   };
 
@@ -44,6 +45,7 @@ class App extends Component {
               path="/inventory"
               render={props => <InventoryPage {...props} categories={categories} />}
             />
+            <Route path="/about" component={AboutPage} />
             <Route path="/gallery" component={Gallery} />
             <Route path="/login" component={CustomerLogin} />
             <Route path="/contact" component={ContactUs} />
