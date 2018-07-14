a4af695db2824beaa7f00e8067516a49573ada2d
Tia Sirrine <tiasirrine@outlook.com>
2018-07-14 09:50:55 -0700

    more changes to contact form

--- client/src/Components/pages/Contact/Form/index.js
+++ client/src/Components/pages/Contact/Form/index.js
@@ -4,9 +4,9 @@ export default class Form extends React.Component {
   state = {
     name: '',
     companyName: '',
-    Email: '',
-    Number: '',
-    Message: ''
+    email: '',
+    number: '',
+    message: ''
   };
 
   render() {
@@ -20,6 +20,8 @@ export default class Form extends React.Component {
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="Name"
+              value={this.state.name}
+              onChange={e => this.setState({ name: e.target.value })}
             />
           </div>
           <div className="form-group">
@@ -29,6 +31,8 @@ export default class Form extends React.Component {
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="Your Comapny"
+              value={this.state.companyName}
+              onChange={e => this.setState({ companyName: e.target.value })}
             />
           </div>
           <div className="form-group">
@@ -38,6 +42,8 @@ export default class Form extends React.Component {
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="name@example.com"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
             />
           </div>
           <div className="form-group">
@@ -47,14 +53,22 @@ export default class Form extends React.Component {
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="888-888-8888"
+              value={this.state.number}
+              onChange={e => this.setState({ number: e.target.value })}
             />
           </div>
 
           <div class="form-group">
             <label for="exampleFormControlTextarea1">Message For Scottsdale Events Decor</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" />
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              value={this.state.message}
+              onChange={e => this.setState({ message: e.target.value })}
+            />
           </div>
-          <button type="submit" className="btn btn-light">
+          <button type="submit" className="btn btn-light" onClick>
             Submit
           </button>
         </div>
