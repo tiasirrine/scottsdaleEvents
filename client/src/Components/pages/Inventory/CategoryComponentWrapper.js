b3b6cdc722a8bedd48a5847d4e44ca2f311c7ef6
Josh <jhint2424@gmail.com>
2018-07-17 21:49:12 -0700

    Update CategoryComponentWrapper.js

--- client/src/Components/pages/Inventory/CategoryComponentWrapper.js
+++ client/src/Components/pages/Inventory/CategoryComponentWrapper.js
@@ -44,30 +44,3 @@ const CategoryComponentWrapper = props => {
 };
 
 export default CategoryComponentWrapper;
-
-<Card
-  className="card-image"
-  style={{
-    backgroundImage:
-      "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')"
-  }}
->
-  <div className="text-white text-center d-flex align-items-center rgba-indigo-strong py-5 px-4">
-    <div>
-      <h5 className="orange-text">
-        <Fa icon="desktop" /> Software
-      </h5>
-      <CardTitle tag="h3" className="pt-2">
-        <strong>This is card title</strong>
-      </CardTitle>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam,
-        voluptatem, optio vero odio nam sit officia accusamus minus error nisi architecto nulla
-        ipsum dignissimos. Odit sed qui, dolorum!
-      </p>
-      <Button color="deep-orange">
-        <Fa icon="clone left" /> View project
-      </Button>
-    </div>
-  </div>
-</Card>;
