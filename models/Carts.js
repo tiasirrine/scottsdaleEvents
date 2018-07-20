c5bc8bf7c8592a3b8d8e505e0569286e0407d573
Trevor Johnson <johnsontrevor55@gmail.com>
2018-07-19 21:33:57 -0700

    fixing bugs with sequelize that took way too long.

--- models/Carts.js
+++ models/Carts.js
@@ -1,21 +1,13 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../config/connection.js');
-
-const Carts = sequelize.define(
-  'carts',
-  {
-    // customer_id: {
-    //   type: Sequelize.INTEGER,
-    //   allowNull: false
-    // },
-    did_check_out: Sequelize.BOOLEAN,
-    allowNull: false
-  },
-  {
-    timestamps: true
-  }
-);
-
-Carts.sync();
-
-module.exports = Carts;
+module.exports = function(sequelize, DataTypes) {
+  const Cart = sequelize.define('Cart', {
+    customer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    did_check_out: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  });
+  return Cart;
+};
