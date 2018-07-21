e41060ebaa429310aee8ae287985617d02451b08
Josh <jhint2424@gmail.com>
2018-07-21 03:15:09 -0700

    deploying

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  production: {
+  test: {
     username: 'root',
     password: null,
     database: 'database_development',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
-  test: {
+  production: {
     username: process.env.JAWSDB_USERNAME,
     password: process.env.JAWSDB_PASSWORD,
     database: process.env.JAWSDB_DATABASE,
