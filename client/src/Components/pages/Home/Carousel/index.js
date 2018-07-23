18c2cb89f5086d62fb9a8b19e7e0b7c2de57edd7
Tia Sirrine <tiasirrine@outlook.com>
2018-07-22 17:32:50 -0700

    styles yo

--- client/src/Components/pages/Home/Carousel/index.js
+++ client/src/Components/pages/Home/Carousel/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-//import images from './Images';
+import images from './Images';
 import { Carousel, CarouselInner, CarouselItem, View, Container } from 'mdbreact';
 
 class CarouselPage extends Component {
   render() {
     return (
       <Container>
-        <h4 className="mt-5 mb-2">Basic example</h4>
+        <br />
         <Carousel
           activeItem={1}
           length={4}
@@ -17,38 +17,22 @@ class CarouselPage extends Component {
           <CarouselInner>
             <CarouselItem itemId="1">
               <View>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
-                  alt="First slide"
-                />
+                <img className="d-block w-100" src={images[0]} alt="First slide" />
               </View>
             </CarouselItem>
             <CarouselItem itemId="2">
               <View>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg"
-                  alt="Second slide"
-                />
+                <img className="d-block w-100" src={images[1]} alt="Second slide" />
               </View>
             </CarouselItem>
             <CarouselItem itemId="3">
               <View>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg"
-                  alt="Third slide"
-                />
+                <img className="d-block w-100" src={images[2]} alt="Third slide" />
               </View>
             </CarouselItem>
             <CarouselItem itemId="4">
               <View>
-                <img
-                  className="d-block w-100"
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20%28143%29.jpg"
-                  alt="Mattonit's item"
-                />
+                <img className="d-block w-100" src={images[3]} alt="Mattonit's item" />
               </View>
             </CarouselItem>
           </CarouselInner>
