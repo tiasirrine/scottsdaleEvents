e6eb9835bfd67da1dfd3e273192f5ad540876df9
Josh <jhint2424@gmail.com>
2018-07-23 23:04:36 -0700

    quantity

--- client/src/Components/pages/Inventory/InventoryCard.js
+++ client/src/Components/pages/Inventory/InventoryCard.js
@@ -56,11 +56,7 @@ class InventoryCard extends Component {
     return (
       <div className="row my-5 pb-4">
         <div className="col-md-5 mb-3 mb-sm-3">
-          <img
-            className="img-fluid product-img"
-            src={image}
-            alt={this.props.cardTitle}
-          />
+          <img className="img-fluid product-img" src={image} alt={this.props.cardTitle} />
         </div>
         <div className="col-md-7 border-bottom pb-3 pb-sm-3">
           <h3 className="mb-2">{this.props.cardTitle}</h3>
@@ -84,7 +80,7 @@ class InventoryCard extends Component {
                 type="number"
                 name="quantity"
                 id="item-quantity"
-                max="1000"
+                max={this.props.cardQuantity}
                 maxLength="4"
                 placeholder={'Quantity'}
               />
