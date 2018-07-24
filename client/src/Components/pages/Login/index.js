10e66eddce5a6a96b275565e6aa58f79ff59e8ab
Tia Sirrine <tiasirrine@outlook.com>
2018-07-23 23:21:41 -0700

    modal for contact page
    Former-commit-id: 214b33a1243cf9cbc369b076dcf58ed92b501fd6

--- client/src/Components/pages/Login/index.js
+++ client/src/Components/pages/Login/index.js
@@ -33,9 +33,7 @@ class Login extends React.Component {
         }
       })
       .catch(err => {
-        const error = err.response
-          ? 'Username or password is incorrect'
-          : 'Connection timed out';
+        const error = err.response ? 'Username or password is incorrect' : 'Connection timed out';
         this.setState({ error: error });
       });
   };
@@ -52,9 +50,7 @@ class Login extends React.Component {
                 <Card>
                   <div className="header pt-3 grey lighten-2">
                     <Row className="d-flex justify-content-start">
-                      <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5">
-                        Log in
-                      </h3>
+                      <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5">Log in</h3>
                     </Row>
                   </div>
                   <CardBody className="mx-4 mt-4">
@@ -77,25 +73,20 @@ class Login extends React.Component {
                     />
                     <p className="font-small grey-text d-flex justify-content-end">
                       Forgot{' '}
-                      <a
-                        href="#"
-                        className="dark-grey-text font-weight-bold ml-1"
-                      >
+                      <a href="#" className="dark-grey-text font-weight-bold ml-1">
                         {' '}
                         Password?
                       </a>
                     </p>
                     {this.state.error && (
-                      <p className="text-danger text-center">
-                        {this.state.error}
-                      </p>
+                      <p className="text-danger text-center">{this.state.error}</p>
                     )}
                     <div className="text-center mb-4 mt-5">
                       <Button
                         onClick={this.onSubmit}
                         color="danger"
                         type="button"
-                        className="btn-block z-depth-2"
+                        className="btn-block z-depth-2 aButton"
                       >
                         Log in
                       </Button>
