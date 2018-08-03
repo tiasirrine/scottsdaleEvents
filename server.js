b98a9f3e9998948f636da422f369069a45a03203
Josh <jhint2424@gmail.com>
2018-08-03 13:12:32 -0700

    fixng deployment

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-// const flash = require('connect-flash');
 const routes = require('./routes/api-routes');
 const session = require('express-session');
 const passport = require('passport');
@@ -13,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // serve static files from /public
-app.use(express.static('./client/public'));
+// app.use(express.static('./client/public'));
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,7 +43,7 @@ app.use(routes);
 // when deployed, if a page refreshes it tries to make a request to the server for that page
 // since this is react our pages are routed dynamically
 // re-directs all requests to serve the home page which will properly load the page
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '/client/public'), function(err) {
     if (err) {
       res.status(500).send(err);
