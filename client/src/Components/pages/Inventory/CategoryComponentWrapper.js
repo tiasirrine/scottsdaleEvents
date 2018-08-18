d385c4d403c210b972e792491d64697daafa66ab
Trevor Johnson <johnsontrevor55@gmail.com>
2018-08-18 11:34:57 -0700

    forgot to save this file
    Former-commit-id: 309c1ced594dbdf66c96efdb2558118fd8f0c9c8

--- client/src/Components/pages/Inventory/CategoryComponentWrapper.js
+++ client/src/Components/pages/Inventory/CategoryComponentWrapper.js
@@ -17,39 +17,36 @@ import {
 
 const CategoryComponentWrapper = props => {
   const { categories, images } = props;
-  // const param = props.match.params.category;
-  console.log(props);
+  const param = props.match.params.category;
   return (
-    <Container>
-      <Row>
-        {categories
-          ? categories.map((a, i) => (
-              <Col md="6" lg="4" xl="3" key={a} className="ind-card-col">
-                <Card className="card card-cascade wider reverse my-4 animated fadeInUpBig w-300">
-                  <Link to={`/inventory/${a}`}>
-                    <div className="view view-cascade overlay">
-                      <View zoom>
-                        <CardImage
-                          cascade
-                          src={images[i]}
-                          className="ind-card-image card-image"
-                          alt="Category Image"
-                        />
-                        <div className="mask flex-center waves-effect waves-light cat-names text-justify" />
-                      </View>
-                    </div>
-                  </Link>
-                </Card>
+    <Row className="justify-content-md-left">
+      {categories
+        ? categories.map((a, i) => (
+            <Col md="6" lg="4" xl="3" key={a} className="ind-card-col">
+              <Card className="card card-cascade wider reverse my-4 animated fadeInUpBig w-300">
                 <Link to={`/inventory/${a}`}>
-                  <figcaption className="figure-caption text-center animated fadeInUpBig">
-                    {a}
-                  </figcaption>
+                  <div className="view view-cascade overlay">
+                    <View zoom>
+                      <CardImage
+                        cascade
+                        src={images[i]}
+                        className="ind-card-image card-image"
+                        alt="Category Image"
+                      />
+                      <div className="mask flex-center waves-effect waves-light cat-names text-justify" />
+                    </View>
+                  </div>
                 </Link>
-              </Col>
-            ))
-          : null}
-      </Row>
-    </Container>
+              </Card>
+              <Link to={`/inventory/${a}`}>
+                <figcaption className="figure-caption text-center animated fadeInUpBig">
+                  {a}
+                </figcaption>
+              </Link>
+            </Col>
+          ))
+        : null}
+    </Row>
   );
 };
 
