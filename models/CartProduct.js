e07a0c91b008bdf2d717344676e549f493998c66
Trevor Johnson <johnsontrevor55@gmail.com>
2018-08-17 22:42:19 -0700

    updated saving quantity for a product

--- models/CartProduct.js
+++ models/CartProduct.js
@@ -2,7 +2,21 @@ module.exports = function(sequelize, DataTypes) {
   const CartProduct = sequelize.define(
     'CartProduct',
     {
-      qty: { type: DataTypes.INTEGER, allowNull: false }
+      qty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          maxQuantity(value) {
+            if (value > this.maxQty) {
+              throw new Error(`Max quantity exceeded`);
+            }
+          }
+        }
+      },
+      maxQty: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
     },
     { timestamps: false }
   );
