5ebcc64e13093027bf01db7446c2a4f49fdfc5b1
Trevor Johnson <johnsontrevor55@gmail.com>
2018-12-08 16:29:35 -0700

    Upgraded react to latest version (#114)

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
+
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
 
